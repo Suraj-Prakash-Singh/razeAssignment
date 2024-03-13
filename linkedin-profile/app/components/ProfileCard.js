7f3174a7dfcc4dafb3a20d412a114530415b13fc
TesnSai <shivamchaudhary8476@gmail.com>
2024-03-14 00:51:19 +0530

    added profile avatar

--- linkedin-profile/app/components/ProfileCard.js
+++ linkedin-profile/app/components/ProfileCard.js
@@ -4,6 +4,7 @@ import bg from "@/public/bg.svg"
 import ProfileCardBtns from "./ProfileCardBtns";
 import ProfileCardEditModal from "./ProfileCardEditModal";
 import profileCardContext from "../utils/profileCardContext";
+import avatar from "@/public/avatar.svg";
 import { useContext, useState } from "react";
 
 const ProfileCard = () => {
@@ -23,10 +24,10 @@ const ProfileCard = () => {
     if(isOpen) return <profileCardContext.Provider value={{profile, updateValue}}><ProfileCardEditModal setIsOpen={() => setIsOpen()}/></profileCardContext.Provider>
     
     return(
-            <div className="w-7/12 h-max mx-40 my-8 rounded-xl flex flex-col shadow-lg">
+            <div className="w-7/12 h-max mx-40 my-8 rounded-xl flex flex-col shadow-lg relative">
                 <Image className="w-full h-2/5 rounded-t-xl" alt="backgroundImg" src={bg}></Image>
                 <div className="w-full h-auto bg-white flex justify-between pb-8 rounded-b-xl">
-                    <div className="w-2/4 h-max mt-8 mx-4">
+                    <div className="w-2/4 h-max mt-16 mx-4">
                         <h1 className="font-semibold text-2xl tracking-wide">{name}</h1>
                         <p>{designation}</p> 
                         <p className="text-gray-500 text-sm my-2">{location} <span className="text-blue-500 font-medium text-sm cursor-pointer hover:underline">Contact info</span></p>
@@ -34,6 +35,9 @@ const ProfileCard = () => {
                         <ProfileCardBtns/>
                     </div>
                     <button className="bg-zinc-300 h-8 mt-6 mr-2 px-4 py-1 rounded-3xl" onClick={handleEdit}>Edit</button>
+                </div>
+                <div className="w-[17%] h-[30%] rounded-[50%] bg-white absolute top-28 left-10 flex items-center justify-center">
+                    <Image src={avatar}></Image>
                 </div>
             </div>
     )
